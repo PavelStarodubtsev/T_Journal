507dcca7c6118d780642d4a0ff5c1a3f8983d688
PavelStar <pvstarodubtsev@gmail.com>
2023-08-15 21:04:37 +0300

    add library next-redux-wrapper,take userData from cookie

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,10 @@
-import type { NextPage } from 'next';
+import type { GetServerSideProps, NextPage } from 'next';
 import { Post } from '../components/Post';
 import { MainLayout } from '../layouts/MainLayout';
+import { wrapper } from '../redux/store';
+import { parseCookies } from 'nookies';
+import { UserApi } from '../utils/api';
+import { setUserData } from '../redux/slices/user';
 
 const Home: NextPage = () => {
   return (
@@ -17,4 +21,21 @@ const Home: NextPage = () => {
   );
 };
 
+// Гидрация - это синхронизация Redux(на серверной части) и Redux(на клиентской части)
+// связываем index.tsx с Redux, теперь получаем корректно данные из Redux
+export const getServerSideProps: GetServerSideProps = wrapper.getServerSideProps((store) => async (ctx) => {
+  try {
+    const { authToken } = parseCookies(ctx);
+
+    const userData = await UserApi.getMe(authToken);
+
+    //@ts-ignore
+    store.dispatch(setUserData(userData));
+
+    return { props: {} };
+  } catch (error) {
+    console.log(error);
+    return { props: {} };
+  }
+});
 export default Home;
